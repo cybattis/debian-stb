6230249a93a3d948aa99078f53ff2a7239a6ec28
Cyril <cybattis@student.42lyon.fr>
2022-01-25 20:48:06 +0100

    fixing bug and typo

--- install.sh
+++ install.sh
@@ -7,14 +7,14 @@ echo "# data drive
 UUID=207585614243FC99 /media/stb47/nexus   ntfs    defaults        0       0" | sudo tee -a /etc/fstab
 mount -a
 
+sudo apt-get update && sudo apt upgrade
 # essential
 sudo aptitude -y install kde-plasma-desktop plasma-nm
-sudo apt-get -y install kde-config-sddm kde-config-sddm
-sudo apt-get -y install nvidia-driver firmware-misc-nonfree mesa-utils dev-essentials python-pip libxext-dev libbsd
+sudo apt-get -y install kde-config-sddm
+sudo aptitude -y python-pip
+sudo apt-get install mesa-utils build-essential libxext-dev -y
 sudo apt -y install ratbagd
-
-# install packages
-sudo apt-get -y install zsh firefox keepassxc qbitorrent guake vlc calibre neovim hplip thunderbird tree htop
+sudo apt-get -y install zsh firefox keepassxc qbittorrent guake vlc calibre neovim hplip thunderbird tree htop
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 #==== add repository ====#
@@ -34,17 +34,18 @@ sudo apt -y install codium
 sudo apt-get -y install spotify-client
 
 # Appimagelauncher
-curl -sS https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
-sudo dpkg -i appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
-rm -f appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
+wget https://github.com/TheAssassin/AppImageLauncher/releases/download/v2.2.0/appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb -o ~/debian-stb/build/appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
+sudo dpkg -i ~/debian-stb/build/appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.deb
+rm -f appimagelauncher_2.2.0-travis995.0f91801.bionic_amd64.debf
 # Slack
-curl -sS https://downloads.slack-edge.com/releases/linux/4.23.0/prod/x64/slack-desktop-4.23.0-amd64.deb -o ~/Debian-stb/desktop-4.23.0-amd64.deb
-sudo dpkg -i desktop-4.23.0-amd64.deb
-rm -f desktop-4.23.0-amd64.deb
+sudo apt-get -y libayatana-appindicator3-1
+wget https://downloads.slack-edge.com/releases/linux/4.23.0/prod/x64/slack-desktop-4.23.0-amd64.deb
+sudo dpkg -i ~/debian-stb/build/slack-desktop-4.23.0-amd64.deb
+rm -f ~/debian-stb/build/desktop-4.23.0-amd64.deb
 # lsd
-curl -sS https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb -o ~/Debian-stb/lsd_0.21.0_amd64.deb
-sudo dpkg -i lsd_0.21.0_amd64.deb
-rm -f lsd_0.21.0_amd64.deb
+wget https://github.com/Peltoche/lsd/releases/download/0.21.0/lsd_0.21.0_amd64.deb
+sudo dpkg -i ~/debian-stb/build/lsd_0.21.0_amd64.deb
+rm -f ~/debian-stb/build/lsd_0.21.0_amd64.deb
 
 # 42
 #norminette
@@ -62,19 +63,23 @@ sudo virsh net-list --all
 sudo adduser $USER libvirt
 sudo adduser $USER libvirt-qemu
 
-ln -s /media/stb47/nexus/Bibliothèque\ calibre .
-ln -s /media/stb47/nexus/Bibliothèque\ lolo .
-ln -s /media/stb47/nexus/Application .
-ln -s /media/stb47/nexus/Photos Images/
-ln -s /media/stb47/nexus/Wallpaper Images/
-ln -s /media/stb47/nexus/Documents\ Administratifs Documents/
-ln -s /media/stb47/nexus/Projects/ .
+ln -s /media/stb47/nexus/Bibliothèque\ calibre ~/
+ln -s /media/stb47/nexus/Bibliothèque\ lolo ~/
+ln -s /media/stb47/nexus/Applications ~/
+ln -s /media/stb47/nexus/Photos ~/Images/
+ln -s /media/stb47/nexus/Wallpaper ~/Images/
+ln -s /media/stb47/nexus/Documents\ Administratifs ~/Documents/
+ln -s /media/stb47/nexus/Projects/ ~/
 
 cp -fR /media/stb47/nexus/Linux-config/.* /home/stb47/
 cp -fR /media/stb47/nexus/Linux-config/* /home/stb47/
 
-cp -Rf dotfiles/nvim /home/$username/.config
+sudo cp -Rf dotfiles/* /home/$username/.config
 
 # closing
 sudo apt-get update && sudo apt-get upgrade
+
+sudo apt-get -y install firmware-misc-nonfree
+sudo apt-get -y install nvidia-driver
+
 sudo reboot
